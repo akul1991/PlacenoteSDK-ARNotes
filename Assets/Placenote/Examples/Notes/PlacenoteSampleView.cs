83e805d20df01dcd89b2a10d1217134fae609b35
Nav Ganti <navganti.12@gmail.com>
2019-02-26 12:02:43 -0500

    First commit for ARNotes

---
+++ Assets/Placenote/Examples/Notes/PlacenoteSampleView.cs
@@ -0,0 +1,361 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using UnityEngine.XR.iOS;
+using System.Runtime.InteropServices;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+public class PlacenoteSampleView : MonoBehaviour, PlacenoteListener
+{
+    [SerializeField] GameObject mMapSelectedPanel;
+    [SerializeField] GameObject mInitPanel;
+    [SerializeField] GameObject mMappingPanel;
+    [SerializeField] GameObject mMapListPanel;
+    [SerializeField] GameObject mExitButton;
+    [SerializeField] GameObject mListElement;
+    [SerializeField] RectTransform mListContentParent;
+    [SerializeField] ToggleGroup mToggleGroup;
+    [SerializeField] Text mLabelText;
+    [SerializeField] Slider mRadiusSlider;
+    [SerializeField] float mMaxRadiusSearch;
+    [SerializeField] Text mRadiusLabel;
+
+    private UnityARSessionNativeInterface mSession;
+
+    private bool mARInit = false;
+    private bool mReportDebug = false;
+
+
+    private LibPlacenote.MapMetadataSettable mCurrMapDetails;
+    private LibPlacenote.MapInfo mSelectedMapInfo;
+
+    private string mSelectedMapId
+    {
+        get
+        {
+            return mSelectedMapInfo != null ? mSelectedMapInfo.placeId : null;
+        }
+    }
+
+    private string mSaveMapId = null;
+
+    // Use this for initialization
+    void Start () {
+        Input.location.Start();
+
+        mMapListPanel.SetActive(false);
+
+        mSession = UnityARSessionNativeInterface.GetARSessionNativeInterface();
+
+        StartARKit();
+        FeaturesVisualizer.EnablePointcloud();
+        LibPlacenote.Instance.RegisterListener(this);
+        ResetSlider();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!mARInit && LibPlacenote.Instance.Initialized()) {
+            mARInit = true;
+            mLabelText.text = "Ready to Start!";
+        }
+    }
+
+    public void OnListMapClick()
+    {
+        if (!LibPlacenote.Instance.Initialized()) {
+            Debug.Log("SDK not yet initialized");
+            return;
+        }
+
+        foreach (Transform t in mListContentParent.transform) {
+            Destroy(t.gameObject);
+        }
+
+        mMapListPanel.SetActive(true);
+        mInitPanel.SetActive(false);
+        mRadiusSlider.gameObject.SetActive(true);
+
+        LibPlacenote.Instance.ListMaps((mapList) =>
+        {
+            // Render the map list!
+            foreach (LibPlacenote.MapInfo mapInfoItem in mapList) {
+                if (mapInfoItem.metadata.userdata != null) {
+                    Debug.Log(mapInfoItem.metadata.userdata.ToString(Formatting.None));
+                }
+
+                AddMapToList(mapInfoItem);
+            }
+        });
+    }
+
+    public void OnRadiusSelect()
+    {
+        Debug.Log("Map search:" + mRadiusSlider.value.ToString("F2"));
+        mLabelText.text = "Filtering maps by GPS location";
+
+        LocationInfo locationInfo = Input.location.lastData;
+
+        float radiusSearch = mRadiusSlider.value * mMaxRadiusSearch;
+        mRadiusLabel.text = "Distance filter: " + (radiusSearch / 1000.0).ToString("F2") + " km";
+
+        LibPlacenote.Instance.SearchMaps(locationInfo.latitude, locationInfo.longitude, radiusSearch, (mapList) =>
+        {
+            // Render the map list!
+            foreach (LibPlacenote.MapInfo mapId in mapList) {
+                if (mapId.metadata.userdata != null) {
+                    Debug.Log(mapId.metadata.userdata.ToString(Formatting.None));
+                }
+
+                AddMapToList(mapId);
+            }
+
+            mLabelText.text = "Map List Complete";
+        });
+    }
+
+    public void ResetSlider()
+    {
+        mRadiusSlider.value = 1.0f;
+        mRadiusLabel.text = "Distance Filter: Off";
+    }
+
+    public void OnCancelClick()
+    {
+        mMapSelectedPanel.SetActive(false);
+        mMapListPanel.SetActive(false);
+        mInitPanel.SetActive(true);
+        ResetSlider();
+    }
+
+    public void OnExitClick()
+    {
+        mInitPanel.SetActive(true);
+        mExitButton.SetActive(false);
+        mMappingPanel.SetActive(false);
+
+        LibPlacenote.Instance.StopSession();
+        FeaturesVisualizer.clearPointcloud();
+        GetComponent<NotesManager>().ClearNotes();
+    }
+
+    void AddMapToList(LibPlacenote.MapInfo mapInfo)
+    {
+        GameObject newElement = Instantiate(mListElement) as GameObject;
+        MapInfoElement listElement = newElement.GetComponent<MapInfoElement>();
+        listElement.Initialize(mapInfo, mToggleGroup, mListContentParent, (value) =>
+        {
+            OnMapSelected(mapInfo);
+        });
+    }
+
+    void OnMapSelected(LibPlacenote.MapInfo mapInfo)
+    {
+        mSelectedMapInfo = mapInfo;
+        mMapSelectedPanel.SetActive(true);
+        mRadiusSlider.gameObject.SetActive(false);
+    }
+
+    public void OnLoadMapClick()
+    {
+        ConfigureSession();
+
+        if (!LibPlacenote.Instance.Initialized()) {
+            Debug.Log("SDK not yet initialized");
+            return;
+        }
+
+        ResetSlider();
+        mLabelText.text = "Loading Map ID: " + mSelectedMapId;
+        LibPlacenote.Instance.LoadMap(mSelectedMapId, (completed, faulted, percentage) =>
+        {
+           if (completed) {
+               mMapSelectedPanel.SetActive(false);
+               mMapListPanel.SetActive(false);
+               mInitPanel.SetActive(false);
+               mExitButton.SetActive(true);
+               mMappingPanel.SetActive(true);
+
+               LibPlacenote.Instance.StartSession(true);
+
+               if (mReportDebug) {
+                   LibPlacenote.Instance.StartRecordDataset((datasetCompleted, datasetFaulted, datasetPercentage) =>
+                   {
+                       if (datasetCompleted) {
+                           mLabelText.text = "Dataset Upload Complete";
+                       } else if (datasetFaulted) {
+                           mLabelText.text = "Dataset Upload Faulted";
+                       } else {
+                           mLabelText.text = "Dataset Upload: " + datasetPercentage.ToString("F2") + "/1.0";
+                       }
+                   });
+                   Debug.Log("Started Debug Report");
+               }
+
+               mLabelText.text = "Loaded ID: " + mSelectedMapId;
+           } else if (faulted) {
+               mLabelText.text = "Failed to load ID: " + mSelectedMapId;
+           } else {
+            mLabelText.text = "Map Download: " + percentage.ToString("F2") + "/1.0";
+           }
+       });
+    }
+
+    public void OnDeleteMapClick()
+    {
+        if (!LibPlacenote.Instance.Initialized()) {
+            Debug.Log("SDK not yet initialized");
+            return;
+        }
+
+        mLabelText.text = "Deleting Map ID: " + mSelectedMapId;
+        LibPlacenote.Instance.DeleteMap(mSelectedMapId, (deleted, errMsg) =>
+        {
+            if (deleted) {
+                mMapSelectedPanel.SetActive(false);
+                mLabelText.text = "Deleted ID: " + mSelectedMapId;
+                OnListMapClick();
+            } else {
+                mLabelText.text = "Failed to delete ID: " + mSelectedMapId;
+            }
+        });
+    }
+
+    public void OnNewMapClick()
+    {
+        ConfigureSession();
+
+        if (!LibPlacenote.Instance.Initialized()) {
+            Debug.Log("SDK not yet initialized");
+            return;
+        }
+
+        mInitPanel.SetActive(false);
+        mMappingPanel.SetActive(true);
+
+        Debug.Log("Started Session");
+        LibPlacenote.Instance.StartSession();
+
+        if (mReportDebug) {
+            if (mReportDebug) {
+                LibPlacenote.Instance.StartRecordDataset((datasetCompleted, datasetFaulted, datasetPercentage) =>
+                {
+                    if (datasetCompleted) {
+                        mLabelText.text = "Dataset Upload Complete";
+                    } else if (datasetFaulted) {
+                        mLabelText.text = "Dataset Upload Faulted";
+                    } else {
+                        mLabelText.text = "Dataset Upload: " + datasetPercentage.ToString("F2") + "/1.0";
+                    }
+                });
+                Debug.Log("Started Debug Report");
+            }
+        }
+
+    }
+
+    private void StartARKit()
+    {
+        #if !UNITY_EDITOR
+        mLabelText.text = "Initializing ARKit"; 
+        Application.targetFrameRate = 60;
+        ConfigureSession();
+        #endif
+    }
+
+    private void ConfigureSession()
+    {
+        #if !UNITY_EDITOR
+        ARKitWorldTrackingSessionConfiguration config = new ARKitWorldTrackingSessionConfiguration();
+        config.alignment = UnityARAlignment.UnityARAlignmentGravity;
+        config.getPointCloudData = true;
+        config.enableLightEstimation = true;
+        config.planeDetection = UnityARPlaneDetection.Horizontal;
+        mSession.RunWithConfig(config);
+        #endif
+    }
+
+    public void OnSaveMapClick()
+    {
+        if (!LibPlacenote.Instance.Initialized()) {
+            Debug.Log("SDK not yet initialized");
+            return;
+        }
+
+        bool useLocation = Input.location.status == LocationServiceStatus.Running;
+        LocationInfo locationInfo = Input.location.lastData;
+
+        mLabelText.text = "Saving...";
+
+        LibPlacenote.Instance.SaveMap((mapId) =>
+        {
+            LibPlacenote.Instance.StopSession();
+            FeaturesVisualizer.clearPointcloud();
+
+            mSaveMapId = mapId;
+            mInitPanel.SetActive(true);
+            mMappingPanel.SetActive(false);
+            mExitButton.SetActive(false);
+
+            LibPlacenote.MapMetadataSettable metadata = new LibPlacenote.MapMetadataSettable();
+            metadata.name = RandomName.Get();
+            mLabelText.text = "Saved Map Name: " + metadata.name;
+
+            JObject userdata = new JObject();
+            metadata.userdata = userdata;
+
+            JObject notesList = GetComponent<NotesManager>().Notes2JSON();
+
+            userdata["notesList"] = notesList;
+            GetComponent<NotesManager>().ClearNotes();
+
+            if (useLocation) {
+                metadata.location = new LibPlacenote.MapLocation();
+                metadata.location.latitude = locationInfo.latitude;
+                metadata.location.longitude = locationInfo.longitude;
+                metadata.location.altitude = locationInfo.altitude;
+            }
+
+            LibPlacenote.Instance.SetMetadata(mapId, metadata, (success) =>
+            {
+                if (success) {
+                    Debug.Log("Meta data successfully saved!");
+                } else {
+                    Debug.Log("Meta data failed to save");
+                }
+            });
+            mCurrMapDetails = metadata;
+        }, (completed, faulted, percentage) =>
+        {
+            if (completed) {
+                mLabelText.text = "Upload Complete:" + mCurrMapDetails.name;
+            } else if (faulted) {
+                mLabelText.text = "Upload of Map Named: " + mCurrMapDetails.name + "faulted";
+            } else {
+                mLabelText.text = "Uploading Map Named: " + mCurrMapDetails.name + "(" + percentage.ToString("F2") + "/1.0)";
+            }
+        });
+    }
+
+    public void OnPose (Matrix4x4 outputPose, Matrix4x4 arkitPose) {}
+
+    public void OnStatusChange(LibPlacenote.MappingStatus prevStatus, LibPlacenote.MappingStatus currStatus)
+    {
+        Debug.Log("prevStatus: " + prevStatus.ToString() + " currStatus: " + currStatus.ToString());
+
+        if (currStatus == LibPlacenote.MappingStatus.RUNNING && prevStatus == LibPlacenote.MappingStatus.LOST) {
+            mLabelText.text = "Localized";
+            GetComponent<NotesManager>().LoadNotesJSON(mSelectedMapInfo.metadata.userdata);
+        } else if (currStatus == LibPlacenote.MappingStatus.RUNNING && prevStatus == LibPlacenote.MappingStatus.WAITING) {
+            mLabelText.text = "Mapping: Tap to add Notes";
+        } else if (currStatus == LibPlacenote.MappingStatus.WAITING) {
+            if (GetComponent<NotesManager>().mNotesObjList.Count != 0) {
+                GetComponent<NotesManager>().ClearNotes();
+            }
+        }
+    }
+}
---
+++ Assets/Placenote/Examples/Notes/Scripts/NotesManager.cs
@@ -0,0 +1,265 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.XR.iOS;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+// Classes to hold Notes information.
+
+[System.Serializable]
+public class NoteInfo
+{
+    public float px;
+    public float py;
+    public float pz;
+    public float qx;
+    public float qy;
+    public float qz;
+    public float qw;
+    public string note;
+}
+
+[System.Serializable]
+public class NotesList
+{
+    // List of all notes stored in the current Place.
+    public NoteInfo[] notes;
+}
+
+
+// Main class for managing notes.
+public class NotesManager : MonoBehaviour {
+
+    public List<NoteInfo> mNotesInfoList = new List<NoteInfo>();
+    public List<GameObject> mNotesObjList = new List<GameObject>();
+
+    // Prefab for the Note
+    public GameObject mNotePrefab;
+
+    private GameObject mCurrNote;
+    private NoteInfo mCurrNoteInfo;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // The HitTest to add a Note
+    bool HitTestWithResultType(ARPoint point, ARHitTestResultType resultTypes)
+    {
+        List<ARHitTestResult> hitResults = UnityARSessionNativeInterface.GetARSessionNativeInterface().HitTest(point, resultTypes);
+    
+        if (hitResults.Count > 0) 
+        {
+            foreach (var hitResult in hitResults) 
+            {
+                Debug.Log("Got hit!");
+
+                Vector3 position = UnityARMatrixOps.GetPosition(hitResult.worldTransform);
+                Quaternion rotation = UnityARMatrixOps.GetRotation(hitResult.worldTransform);
+
+                // Transform to Placenote frame of reference (planes are detected in ARKit frame of reference)
+                Matrix4x4 worldTransform = Matrix4x4.TRS(position, rotation, Vector3.one);
+                Matrix4x4? placenoteTransform = LibPlacenote.Instance.ProcessPose(worldTransform);
+
+                Vector3 hitPosition = PNUtility.MatrixOps.GetPosition(placenoteTransform.Value);
+                Quaternion hitRotation = PNUtility.MatrixOps.GetRotation(placenoteTransform.Value);
+
+                // Create note
+                InstantiateNote(hitPosition);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Update checks for hit test.
+    void Update ()
+    {
+        // Check if the screen is touched.
+        #if UNITY_EDITOR
+        // For simulation in the editor.
+        if (Input.GetMouseButtonDown(0))
+        { 
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Get hit transform
+                Vector3 position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+
+                // Create note.
+                InstantiateNote(position);
+            }
+        }
+
+        #else
+        // For hit testing on the device.
+
+        if (Input.touchCount > 0) 
+        {
+            var touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (EventSystem.current.currentSelectedGameObject == null)
+                {
+                    Debug.Log("Not touching a UI button, moving on.");
+
+                    // Test if you are hitting an existing marker
+                    RaycastHit hit = new RaycastHit();
+                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
+
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        Debug.Log("Selected an existing note, opening for editing.");
+                        mCurrNote = hit.transform.gameObject;
+
+                        editCurrNote();
+                    }
+                    else
+                    {
+                        // Add new note.
+                        var screenPosition = Camera.main.ScreenToViewportPoint(touch.position);
+                        ARPoint point = new ARPoint
+                        {
+                            x = screenPosition.x,
+                            y = screenPosition.y
+                        };
+
+                        ARHitTestResultType[] resultTypes =
+                        {
+                            ARHitTestResultType.ARHitTestResultTypeFeaturePoint
+                        };
+
+                        foreach (ARHitTestResultType resultType in resultTypes)
+                        {
+                            if (HitTestWithResultType(point, resultType))
+                            {
+                                Debug.Log("Found a hit test result");
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        #endif
+    }
+
+
+    public void InstantiateNote(Vector3 notePosition)
+    {
+
+        // Instantiate new note prefab and set transform.
+        GameObject note = Instantiate(mNotePrefab);
+        note.transform.position = notePosition;
+        note.transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
+
+        // Turn the note to point at the camera
+        Vector3 targetPosition = new Vector3(Camera.main.transform.position.x,
+                                             Camera.main.transform.position.y,
+                                             Camera.main.transform.position.z);
+        note.transform.LookAt(targetPosition);
+        note.transform.Rotate(0f, -180f, 0f);
+
+        // Set currently selected note
+        mCurrNote = note;
+        
+        NoteInfo noteInfo = new NoteInfo
+        {
+            px = note.transform.position.x,
+            py = note.transform.position.y,
+            pz = note.transform.position.z,
+            qx = note.transform.rotation.x,
+            qy = note.transform.rotation.y,
+            qz = note.transform.rotation.z,
+            qw = note.transform.rotation.w,
+            note = ""
+        };
+
+        mNotesInfoList.Add(noteInfo);
+        mNotesObjList.Add(note);
+        editCurrNote();
+    }
+
+    private void editCurrNote()
+    {
+   
+        InputField input = mCurrNote.GetComponentInChildren<InputField>();
+
+        input.ActivateInputField();
+    }
+
+    public GameObject NoteFromInfo(NoteInfo info)
+    {
+        GameObject note = Instantiate(mNotePrefab);
+        note.transform.position = new Vector3(info.px, info.py, info.pz);
+        note.transform.rotation = new Quaternion(info.qx, info.qy, info.qz, info.qw);
+        note.transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
+
+        note.GetComponentInChildren<InputField>().text = info.note;
+
+        return note;
+    }
+
+    public void ClearNotes()
+    {
+        foreach (var obj in mNotesObjList)
+        {
+            Destroy(obj);
+        }
+
+        mNotesObjList.Clear();
+        mNotesInfoList.Clear();
+    }
+
+    public JObject Notes2JSON()
+    {
+        NotesList notesList = new NotesList
+        {
+            notes = new NoteInfo[mNotesInfoList.Count]
+        };
+
+        for (int i = 0; i < mNotesInfoList.Count; ++i)
+        {
+            // Grab text from object at save time.
+            mNotesInfoList[i].note = mNotesObjList[i].GetComponentInChildren<InputField>().text;
+
+            notesList.notes[i] = mNotesInfoList[i];
+        }
+
+        return JObject.FromObject(notesList);
+    }
+
+    public void LoadNotesJSON(JToken mapMetadata)
+    {
+        ClearNotes();
+
+        if (mapMetadata is JObject && mapMetadata["notesList"] is JObject)
+        {
+            NotesList notesList = mapMetadata["notesList"].ToObject<NotesList>();
+
+            if (notesList.notes == null)
+            {
+                Debug.Log("No notes created!");
+                return;
+            }
+
+            foreach (var noteInfo in notesList.notes)
+            {
+                mNotesInfoList.Add(noteInfo);
+
+                GameObject note = NoteFromInfo(noteInfo);
+
+                mNotesObjList.Add(note);
+            }
+        }
+    }
+}
